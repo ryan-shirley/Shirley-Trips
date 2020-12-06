45426aebbc43a0d18e093b165a4b3eefa3e32bea
Ryan Shirley <ryanjshirley@gmail.com>
2020-12-06 18:39:36 +0000

    Secure app requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
